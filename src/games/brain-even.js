72d1a4cf576a03d13659a6844f214434ce2e7a6b
nmikhaylov <nmikhaylov92@gmail.com>
2019-04-08 16:45:32 +0300

    Add BrainEven Game

---
+++ src/games/brain-even.js
@@ -0,0 +1,30 @@
+import getName from '..';
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const isEven = num => num % 2 === 0;
+const boolToWord = bool => (bool ? 'yes' : 'no');
+
+export default () => {
+  const gameMessage = '\nAnswer "yes" if number even otherwise answer "no".';
+  const userName = getName(gameMessage);
+  const correctAnswerString = ['yes', 'no'];
+  const rounds = 3;
+
+  for (let i = 0; i < rounds; i += 1) {
+    const num = getRandomNumber(0, 100);
+    const userAnswer = readlineSync.question(`Question: ${num}\nYour answer: `);
+    const boolAnswer = userAnswer === 'yes';
+
+    if (isEven(num) !== boolAnswer || !correctAnswerString.includes(userAnswer)) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${boolToWord(!userAnswer)}`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+
+    console.log('Correct');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+  return true;
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  const name = readlineSync.question('Welcome to the Brain Games!\n\nMay I have your name? ');
-  console.log(`Hello, ${name}`);
+export default (gameMessage) => {
+  const welcomeMessage = (`Welcome to the Brain Games!${gameMessage || ''}`);
+  const name = readlineSync.question(`${welcomeMessage}\n\nMay I have your name? `);
+
+  console.log(`Hello, ${name}\n`);
+  return name;
 };
